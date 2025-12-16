93b515eb2f7880112f35045dc87867052fbc840c
ZeebFly <zeebrobot@gmail.com>
2025-12-16 14:20:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::post('/order', [OrderController::class, 'store']);
 Route::get('/order/{id}', [OrderController::class, 'show']);
+Route::post('/tripay/callback', [PaymentController::class, 'callback']);
