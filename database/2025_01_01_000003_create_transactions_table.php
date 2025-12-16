333afba76d751a248f6bca52ac87931c963b3bce
ZeebFly <zeebrobot@gmail.com>
2025-12-16 14:08:46 +0700

    Update 2025_01_01_000003_create_transactions_table.php

--- database/2025_01_01_000003_create_transactions_table.php
+++ database/2025_01_01_000003_create_transactions_table.php
@@ -1,1 +1,25 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice')->unique();
+            $table->string('user_id');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->string('payment_status')->default('pending');
+            $table->string('topup_status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
