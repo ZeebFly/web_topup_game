3b35377604d239b533134accc95fad4c06c9952c
ZeebFly <zeebrobot@gmail.com>
2025-12-16 14:03:18 +0700

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
+
+Route::get('/', [HomeController::class, 'index']);
+Route::post('/order', [OrderController::class, 'store']);
+Route::get('/order/{id}', [OrderController::class, 'show']);
