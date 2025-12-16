248c5d81d4a769da1b4a68eb5e23733226d76d2f
ZeebFly <zeebrobot@gmail.com>
2025-12-16 14:08:06 +0700

    Update 2025_01_01_000002_create_products_table.php

--- database/2025_01_01_000002_create_products_table.php
+++ database/2025_01_01_000002_create_products_table.php
@@ -1,1 +1,24 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('game_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('sku')->unique();
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
